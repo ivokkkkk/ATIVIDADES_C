5aada5c936de8007a3a012d58cec6d1537966452
ivokkkkk <pedroivo9141@outlook.com>
2024-08-07 20:56:26 -0300

    Add files via upload

---
+++ exc_1_aula_c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int a,b,soma;
+
+printf("informe dois numeros interios:");
+scanf("%d",&a);
+scanf("%d",&b);
+
+soma=a+b;
+
+printf("A soma de %d e %d é %d", a,b,soma);
+}
